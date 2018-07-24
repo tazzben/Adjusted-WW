6776ca2f334205beea65e31ae4fb3a6201ef0621
Ben Smith <ben@wbpsystems.com>
2018-07-24 16:29:59 -0500

    Add multiprocessor support
    This update allows the simulation to use all of the processors on the users computer.  This allows for much faster execution.

--- Simulation.py
+++ Simulation.py
@@ -6,6 +6,8 @@ import csv
 import numpy
 import pandas as pd
 import argparse
+from multiprocessing import Pool
+from itertools import product
 
 
 
@@ -24,17 +26,7 @@ parseresults = parser.parse_args()
 if parseresults.opts>0:
     numoptions = parseresults.opts
 
-
-def Known(x):
-    global numoptions
-    global mu
-    if x['draw'] <= mu:
-        return 1
-    else:
-        return 0
-
-def GuessPre(x):
-    global numoptions
+def GuessPre(x,numoptions):
     if x['pretest']==1:
         return 1
     if x['guesspre'] <= 1.0/numoptions:
@@ -42,8 +34,7 @@ def GuessPre(x):
     else:
         return 0
 
-def GuessPost(x):
-    global numoptions
+def GuessPost(x,numoptions):
     if x['posttest']==1:
         return 1
     if x['guesspost'] <= 1.0/numoptions:
@@ -51,48 +42,28 @@ def GuessPost(x):
     else:
         return 0
 
-def NL(x):
-    if x['posttest']==0 and x['pretest']==1:
-        return 1
-    else:
-        return 0
+Known = lambda x, mu: 1 if x['draw'] <= mu else 0
+NL = lambda x: 1 if x['posttest']==0 and x['pretest']==1 else 0
+PL = lambda x: 1 if x['posttest']==1 and x['pretest']==0 else 0
+ZL = lambda x: 1 if x['posttest']==0 and x['pretest']==0 else 0
+RL = lambda x: 1 if x['posttest']==1 and x['pretest']==1 else 0
 
-def PL(x):
-    if x['posttest']==1 and x['pretest']==0:
-        return 1
-    else:
-        return 0
-
-def ZL(x):
-    if x['posttest']==0 and x['pretest']==0:
-        return 1
-    else:
-        return 0
-
-def RL(x):
-    if x['posttest']==1 and x['pretest']==1:
-        return 1
-    else:
-        return 0
-
-def GenerateClass(students):
+def GenerateClass(students,mu,numoptions):
     df = pd.DataFrame({'draw':numpy.random.uniform(0,1,students).tolist(),'guesspre':numpy.random.uniform(0,1,students).tolist(),'guesspost':numpy.random.uniform(0,1,students).tolist(),'pretest':0,'posttest':0,'pl':0,'nl':0,'zl':0,'rl':0})
-    df['pretest'] = df.apply(Known, axis=1)
+    df['pretest'] = df.apply(Known, axis=1, args=(mu,))
     df['posttest'] = df['pretest']
-    df['pretest'] = df.apply(GuessPre, axis=1)
-    df['posttest'] = df.apply(GuessPost, axis=1)
+    df['pretest'] = df.apply(GuessPre, axis=1, args=(numoptions,))
+    df['posttest'] = df.apply(GuessPost, axis=1, args=(numoptions,))
     df['pl'] = df.apply(PL, axis=1)
     df['nl'] = df.apply(NL, axis=1)
     df['zl'] = df.apply(ZL, axis=1)
     df['rl'] = df.apply(RL, axis=1)
     return (df['pl'].mean(),df['nl'].mean(),df['zl'].mean(),df['rl'].mean())
 
-def Simulation(csize):
-    global mu
-    global numoptions
+def Simulation(csize,mu,numoptions,R):
     world = pd.DataFrame(columns=('pl', 'nl', 'zl','rl','gamma','alpha','mu','flow','gain'))
-    for i in range(10000):
-        pl, nl, zl, rl = GenerateClass(csize)
+    for i in range(R):
+        pl, nl, zl, rl = GenerateClass(csize,mu,numoptions)
         emu = ((nl+rl)-1)/(numoptions-1)+nl+rl
         egamma = (numoptions*(nl+pl*numoptions+rl-1))/((numoptions-1)**2)
         ealpha = (numoptions*(nl*numoptions+pl+rl-1))/((numoptions-1)**2)
@@ -106,68 +77,86 @@ def Simulation(csize):
 
 
 
+def ManageProcess(row):
+    cs = row['cs']
+    mu = row['mu']
+    R = row['R']
+    numoptions = row['numoptions']
+    print('Simulation Class: ' + str(cs) + ' mu: ' + str(mu) + ' with ' + str(numoptions) + ' options')
+    r = Simulation(cs,mu,numoptions,R)
+    
+    ng =  r[r['gain'] <= r['gain'].quantile(q=.90)]
+    ng = ng.sort_values(['gain','mu'], ascending=[False,True])
+    ning = ng.iloc[0]['gain']
+    
+    ng =  r[r['gain'] <= r['gain'].quantile(q=.95)]
+    ng = ng.sort_values(['gain','mu'], ascending=[False,True])
+    ninfg = ng.iloc[0]['gain']
+    
+    rgain = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
+    
+    
+    ng =  r[r['gamma'] <= r['gamma'].quantile(q=.90)]
+    ng = ng.sort_values(['gamma','mu'], ascending=[False,True])
+    ning = ng.iloc[0]['gamma']
+    ng =  r[r['gamma'] <= r['gamma'].quantile(q=.95)]
+    ng = ng.sort_values(['gamma','mu'], ascending=[False,True])
+    ninfg = ng.iloc[0]['gamma']
+    
+    
+    rgamma = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
+    
+    
+    ng =  r[r['alpha'] <= r['alpha'].quantile(q=.90)]
+    ng = ng.sort_values(['alpha','mu'], ascending=[False,True])
+    ning = ng.iloc[0]['alpha']
+    
+    ng =  r[r['alpha'] <= r['alpha'].quantile(q=.95)]
+    ng = ng.sort_values(['alpha','mu'], ascending=[False,True])
+    ninfg = ng.iloc[0]['alpha']
+    
+    
+    ralpha = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
+    
+    ng =  r[r['flow'] <= r['flow'].quantile(q=.90)]
+    ng = ng.sort_values(['flow','mu'], ascending=[False,True])
+    ning = ng.iloc[0]['flow']
+    
+    
+    ng =  r[r['flow'] <= r['flow'].quantile(q=.95)]
+    ng = ng.sort_values(['flow','mu'], ascending=[False,True])
+    ninfg = ng.iloc[0]['flow']
+    
+    rflow = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
+    return {'gain':rgain, 'gamma':rgamma, 'alpha':ralpha, 'flow':rflow}
+
+
+
+classSize = [15,20,25,30,35,40,45,50,60,70,80,90,100,150,200,250,300]
+muList = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
+R = 10000
+combs=pd.DataFrame(list(product(classSize,muList)), columns=['cs', 'mu'])
+combs['R']=R
+combs['numoptions']=numoptions
+interList = []
+for _, row in combs.iterrows():
+    interList.append({'cs':row['cs'].astype(int),'mu':row['mu'].astype(float),'R':row['R'].astype(int),'numoptions':row['numoptions'].astype(int)})
+
+p = Pool()
+r = p.map(ManageProcess, interList)
+p.close()
+p.join()
+
 dgamma = pd.DataFrame(columns=('mu', 'class', 'q90','q95','ci25','ci975'))
 dalpha = pd.DataFrame(columns=('mu', 'class', 'q90','q95','ci25','ci975'))
 dflow = pd.DataFrame(columns=('mu', 'class', 'q90','q95','ci25','ci975'))
 dgain = pd.DataFrame(columns=('mu', 'class', 'q90','q95','ci25','ci975'))
 
-for cs in [15,20,25,30,35,40,45,50,60,70,80,90,100,150,200,250,300]:
-    for cmu in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]:
-        global mu
-        mu = cmu
-        print('Simulation Class: ' + str(cs) + ' mu: ' + str(mu) + ' with ' + str(numoptions) + ' options')
-        r = Simulation(cs)
-
-        ng =  r[r['gain'] <= r['gain'].quantile(q=.90)]
-        ng = ng.sort_values(['gain','mu'], ascending=[False,True])
-        ning = ng.iloc[0]['gain']
-        
-        ng =  r[r['gain'] <= r['gain'].quantile(q=.95)]
-        ng = ng.sort_values(['gain','mu'], ascending=[False,True])
-        ninfg = ng.iloc[0]['gain']
-        
-        rgain = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
-        dgain = dgain.append(rgain,ignore_index=True)
-        
-        
-        ng =  r[r['gamma'] <= r['gamma'].quantile(q=.90)]
-        ng = ng.sort_values(['gamma','mu'], ascending=[False,True])
-        ning = ng.iloc[0]['gamma']
-
-
-        ng =  r[r['gamma'] <= r['gamma'].quantile(q=.95)]
-        ng = ng.sort_values(['gamma','mu'], ascending=[False,True])
-        ninfg = ng.iloc[0]['gamma']
-
-
-        rgamma = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
-        dgamma = dgamma.append(rgamma,ignore_index=True)
-
-
-        ng =  r[r['alpha'] <= r['alpha'].quantile(q=.90)]
-        ng = ng.sort_values(['alpha','mu'], ascending=[False,True])
-        ning = ng.iloc[0]['alpha']
-
-        ng =  r[r['alpha'] <= r['alpha'].quantile(q=.95)]
-        ng = ng.sort_values(['alpha','mu'], ascending=[False,True])
-        ninfg = ng.iloc[0]['alpha']
-
-
-        ralpha = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
-        dalpha = dalpha.append(ralpha,ignore_index=True)
-
-        ng =  r[r['flow'] <= r['flow'].quantile(q=.90)]
-        ng = ng.sort_values(['flow','mu'], ascending=[False,True])
-        ning = ng.iloc[0]['flow']
-
-
-        ng =  r[r['flow'] <= r['flow'].quantile(q=.95)]
-        ng = ng.sort_values(['flow','mu'], ascending=[False,True])
-        ninfg = ng.iloc[0]['flow']
-
-        rflow = {'mu':mu, 'class':cs, 'q90':ning, 'q95':ninfg, 'ci25':r['mu'].quantile(q=.025), 'ci975':r['mu'].quantile(q=.975)}
-        dflow = dflow.append(rflow,ignore_index=True)
-
+for row in r:
+    dgamma = dgamma.append(row['gamma'],ignore_index=True)
+    dalpha = dalpha.append(row['alpha'],ignore_index=True)
+    dflow = dflow.append(row['flow'],ignore_index=True)
+    dgain = dgain.append(row['gain'],ignore_index=True)
 
 dgamma.to_csv('GammaResults.csv')
 dalpha.to_csv('AlphaResults.csv')
